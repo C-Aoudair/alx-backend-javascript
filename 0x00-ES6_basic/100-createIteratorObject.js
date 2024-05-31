92ba89ce0c45ac415fe58b982aba477cb10ca036
C-Aoudair <aoudair16031999@gmail.com>
2024-06-01 00:28:26 +0100

    clean the folder

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,30 @@
+// export default function createIteratorObject(report) {
+//   return (function* generator() {
+//     for (const department of Object.values(report.allEmployees)) {
+//       for (const employee of department) {
+//         yield employee;
+//       }
+//     }
+//   }());
+// }
+
 export default function createIteratorObject(report) {
-  return (function* generator() {
-    for (const department of Object.values(report.allEmployees)) {
-      for (const employee of department) {
-        yield employee;
-      }
-    }
-  }());
+  const employees = Object.values(report.allEmployees);
+  const employeesList = [];
+  for (const employee of employees) {
+    employeesList.push(...employee);
+  }
+  let index = 0;
+  return {
+    [Symbol.iterator]: () => ({
+      next: () => {
+        if (index < employeesList.length) {
+          const value = employeesList[index];
+          index += 1;
+          return { value, done: false };
+        }
+        return { done: true };
+      },
+    }),
+  };
 }
