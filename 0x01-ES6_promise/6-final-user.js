4099080184e2ca40d778c5633253f832596acf7b
C-Aoudair <aoudair16031999@gmail.com>
2024-06-01 12:56:48 +0100

    add task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,27 @@
+import createUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([createUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      const myResults = [];
+      results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          myResults.push(
+            {
+              status: result.status,
+              value: result.value,
+            },
+          );
+        } else {
+          myResults.push(
+            {
+              status: result.status,
+              value: result.reason,
+            },
+          );
+        }
+      });
+      return myResults;
+    });
+}
