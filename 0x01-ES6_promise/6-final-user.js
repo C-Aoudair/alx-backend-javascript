c80962b21282a89a07b6118b05ee9a8258cac0dc
C-Aoudair <aoudair16031999@gmail.com>
2024-06-01 14:12:14 +0100

    fix task 6 style

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,27 +3,10 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([createUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      const myResults = [];
-      results.forEach((result) => {
-        if (result.status === 'fulfilled') {
-          myResults.push(
-            {
-              status: result.status,
-              value: result.value,
-            },
-          );
-        } else {
-          myResults.push(
-            {
-              status: result.status,
-              value: String(result.reason),
-            },
-          );
-        }
-      });
-      return myResults;
-    });
+    .then((results) => (
+      results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : String(result.reason),
+      }))
+    ));
 }
-
-
