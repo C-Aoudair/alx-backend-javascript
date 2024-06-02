0e3246a3acbb3f8ebe65cdd1f315425623c53163
C-Aoudair <aoudair16031999@gmail.com>
2024-06-02 01:11:29 +0100

    fix task 9

--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -1,6 +1,9 @@
 import listOfStudents from "./9-hoisting.js";
+import { HolbertonClass, StudentHolberton } from "./9-hoisting.js";
 
 console.log(listOfStudents);
+console.log((listOfStudents[0] instanceof StudentHolberton));
+console.log(listOfStudents[0].holbertonClass instanceof HolbertonClass);
 
 const listPrinted = listOfStudents.map(
     student => student.fullStudentDescription
