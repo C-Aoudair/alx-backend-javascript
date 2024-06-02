2ca562e04fa9c2c2ea8c21bfe116dac67733becf
C-Aoudair <aoudair16031999@gmail.com>
2024-06-02 01:21:21 +0100

    add task 10

--- 0x02-ES6_classes/main.js
+++ 0x02-ES6_classes/main.js
@@ -1,12 +1,14 @@
-import listOfStudents from "./9-hoisting.js";
-import { HolbertonClass, StudentHolberton } from "./9-hoisting.js";
+import Car from "./10-car.js";
 
-console.log(listOfStudents);
-console.log((listOfStudents[0] instanceof StudentHolberton));
-console.log(listOfStudents[0].holbertonClass instanceof HolbertonClass);
+class TestCar extends Car {}
 
-const listPrinted = listOfStudents.map(
-    student => student.fullStudentDescription
-);
+const tc1 = new TestCar("Nissan", "Turbo", "Pink");
+const tc2 = tc1.cloneCar();
 
-console.log(listPrinted)
+console.log(tc1);
+console.log(tc1 instanceof TestCar);
+
+console.log(tc2);
+console.log(tc2 instanceof TestCar);
+
+console.log(tc1 == tc2);
