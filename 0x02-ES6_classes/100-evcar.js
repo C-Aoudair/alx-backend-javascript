106e6933549263ea1488cd90b9b9392e4af54659
C-Aoudair <aoudair16031999@gmail.com>
2024-06-02 02:01:37 +0100

    add task 100

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,21 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(range) {
+    this._range = range;
+  }
+
+  cloneCar() {
+    const SuperConstructor = Object.getPrototypeOf(this.constructor);
+    return new SuperConstructor();
+  }
+}
