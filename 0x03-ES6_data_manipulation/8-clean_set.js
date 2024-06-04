5a50acc0228d189e31c808a4feb5bc1e31a65b02
C-Aoudair <aoudair16031999@gmail.com>
2024-06-04 14:05:54 +0100

    add another edje case ot task 8: if the set not a Set object

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === '' || typeof startString !== 'string') return '';
+  if (startString === '' || typeof startString !== 'string' || typeof set !== 'set') return '';
 
   const newString = [];
   set.forEach((myString) => {
