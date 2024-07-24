885fc3f51953f45808c1ac70c7f6d11d7705bdb5
C-Aoudair <aoudair16031999@gmail.com>
2024-07-24 14:15:11 +0100

    add 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -37,7 +37,8 @@ const app = createServer((request, response) => {
   } else if (url === '/students') {
     countStudents(process.argv[2])
       .then((result) => {
-        let message = `Number of students: ${result.numOfStudents}\n`;
+	let message = 'This is the list of our students\n'
+        message += `Number of students: ${result.numOfStudents}\n`;
         for (const [field, { count, list }] of Object.entries(result.fields)) {
           message += `Number of students in ${field}: ${count}. List: ${list.join(', ')}\n`;
         }
