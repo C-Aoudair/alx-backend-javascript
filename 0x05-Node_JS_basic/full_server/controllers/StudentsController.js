4b432f5630ef45ea62d16ee1676e04485a54f074
C-Aoudair <aoudair16031999@gmail.com>
2024-07-25 16:56:56 +0100

    test

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -6,12 +6,12 @@ export default class StudentsController {
     readDatabase(PATH)
       .then((result) => {
         let message = "This is the list of our students\n";
-        Object.keys(result).forEach((field) => {
+        Object.keys(result).sort().forEach((field) => {
           const list = result[field];
           message += `Number of students in ${field}: ${list.length}. List: ${list.join(", ")}\n`;
         });
         response.status(200);
-        response.send(message);
+        response.send(message.slice(0, -1));
       })
       .catch((error) => {
         response.status(500);
@@ -21,14 +21,14 @@ export default class StudentsController {
 
   static getAllStudentsByMajor(request, response) {
     const major = request.params.major;
-    if (major !== "CS" || major !== "SWE") {
+    if (major !== "CS" && major !== "SWE") {
       response.status(500);
       response.send("Major parameter must be CS or SWE");
     } else {
       const PATH = process.argv.length > 2 ? process.argv[2] : "";
       readDatabase(PATH)
         .then((result) => {
-          const message = `List: ${field}: ${result[major].join(", ")}\n`;
+          const message = `List: ${result[major].join(", ")}`;
           response.status(200);
           response.send(message);
         })
--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -7,3 +7,5 @@ app.use(router);
 app.listen(1245, () => {
 	console.log('Server running on port 1245');
 });
+
+export default app;
